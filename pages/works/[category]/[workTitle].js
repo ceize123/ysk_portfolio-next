bb834b1991798bb630337c406f72a6d30b940a64
ytkuo <hahaeating1@gmail.com>
2022-07-09 10:49:33 -0400

    7/9 test static props

--- pages/works/[category]/[workTitle].js
+++ pages/works/[category]/[workTitle].js
@@ -8,33 +8,36 @@ import findWork from "../../../components/FindWork";
 import { useShareWorks } from "../../../components/ShareStates";
 import { useBetween } from "use-between";
 import { useRouter } from "next/router";
+import dbConnect from "../../../util/connection";
+import Category from "../../../models/Category";
+import Work from "../../../models/Work";
 
-function Work() {
+function WorkDetail({work}) {
 	const pathname = useRouter();
 	const { category, workTitle } = pathname.query;
 	const { works } = useBetween(useShareWorks);
-	const [work, setWork] = useState();
+	// const [work, setWork] = useState();
 
-	useEffect(() => {
-		if (works && category && workTitle) {
-			const cate = findCat(works, category);
-			const w = findWork(cate, workTitle);
+	// useEffect(() => {
+	// 	if (works && category && workTitle) {
+	// 		const cate = findCat(works, category);
+	// 		const w = findWork(cate, workTitle);
 
-			const ul = document.querySelector("ul");
-			ul.style.backgroundColor = w.navColor;
-			ul.style.color = w.navTextColor;
-			setWork(w);
-		}
-	}, [works, work, category, workTitle]);
+	// 		const ul = document.querySelector("ul");
+	// 		ul.style.backgroundColor = w.navColor;
+	// 		ul.style.color = w.navTextColor;
+	// 		setWork(w);
+	// 	}
+	// }, [works, work, category, workTitle]);
 
 	// console.log(works[category]);
 	// const [work, setWork] = useState(works[category])
 
-	// useEffect(() => {
-	// 	const ul = document.querySelector("nav ul");
-	// 	ul.style.color = work.navTextColor;
-	// 	ul.style.backgroundColor = work.navColor;
-	// }, [work]);
+	useEffect(() => {
+		const ul = document.querySelector("nav ul");
+		ul.style.color = work.navTextColor;
+		ul.style.backgroundColor = work.navColor;
+	}, [work]);
 
 	return (
 		<div className="front-end">
@@ -56,48 +59,60 @@ function Work() {
 	);
 }
 
-export default Work;
+export default WorkDetail;
 
-// export async function getStaticPaths() {
-// 	// const response = await fetch("http://localhost:3000/api/works");
-// 	const response = await fetch(`${process.env.URL}/api/works`);
-// 	const data = await response.json();
-// 	let dataArray = [];
+export async function getStaticPaths() {
+	// const response = await fetch("http://localhost:3000/api/works");
+	// const response = await fetch(`${process.env.URL}/api/works`);
 
-// 	data.map(category => {	
-// 		category.works.map(work => {
-// 			const param = { category: category.category, title: work.title };
-// 			dataArray.push(param);
-// 		});
-// 	});
+	await dbConnect();
+	const response = await Category.find();
+	// const response = await fetch(`${process.env.URL}/api/works`);
+	// const data = await response.json();
 
-// 	const paths = dataArray.map(item => {
-// 		return {
-// 			params: {
-// 				category: `${item.category}`,
-// 				workTitle: `${item.title}`
-// 			}
-// 		};
-// 	});
+	const data = await JSON.parse(JSON.stringify(response));
+	let dataArray = [];
 
-// 	return {
-// 		paths,
-// 		fallback: false,
-// 	};
-// }
+	data.map(category => {	
+		category.works.map(work => {
+			const param = { category: category.category, title: work.title };
+			dataArray.push(param);
+		});
+	});
 
-// export async function getStaticProps(context) {
-// 	const { params } = context;
-// 	const { category, workTitle } = params;
-// 	// const response = await fetch(`http://localhost:3000/api/works/${workTitle}`);
-// 	const response = await fetch(`${process.env.URL}/api/works/${workTitle}`);
-// 	const data = await response.json();
-// 	const work = data;
+	const paths = dataArray.map(item => {
+		return {
+			params: {
+				category: `${item.category}`,
+				workTitle: `${item.title}`
+			}
+		};
+	});
 
-// 	return {
-// 		props: {
-// 			category: category,
-// 			work,
-// 		},
-// 	};
-// }
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps(context) {
+	const { params } = context;
+	const { category, workTitle } = params;
+	// const response = await fetch(`http://localhost:3000/api/works/${workTitle}`);
+	// const response = await fetch(`${process.env.URL}/api/works/${workTitle}`);
+
+	await dbConnect();
+	const response = await Work.findOne({ "title": workTitle });
+	// const response = await fetch(`${process.env.URL}/api/works`);
+	// const data = await response.json();
+
+	const data = await JSON.parse(JSON.stringify(response));
+	const work = data;
+
+	return {
+		props: {
+			category: category,
+			work,
+		},
+	};
+}
