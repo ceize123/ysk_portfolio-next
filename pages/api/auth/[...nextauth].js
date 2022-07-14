11f69f11ef20ad4da648e2838caf1b5b62eac873
ytkuo <hahaeating1@gmail.com>
2022-07-13 21:18:19 -0400

    7/13 auth test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,13 +4,10 @@ import GitHubProvider from "next-auth/providers/github";
 export default NextAuth({
 	providers: [
 		GitHubProvider({
-			secret: process.env.SECRET,
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET
 		})
 	],
 	database: process.env.MONGO_URL,
-	session: {
-		jwt: true // ask next auth library to use jwt for session management, not db
-	},
+	secret: process.env.SECRET,
 });
