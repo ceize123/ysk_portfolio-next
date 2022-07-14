620130eca7eac65958047bd7360d5e8029f09f0c
ytkuo <hahaeating1@gmail.com>
2022-07-13 22:11:27 -0400

    7/13 auth test2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,11 +3,12 @@ import GitHubProvider from "next-auth/providers/github";
 
 // https://medium.com/@prodmxle/setting-up-nextauth-js-application-and-deploying-it-via-vercel-d6b02bf98397
 export default NextAuth({
+	site: process.env.NEXTAUTH_URL,
 	providers: [
 		GitHubProvider({
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET
 		})
 	],
-	secret: "dCVdSHc8MRF@uFyqAS4g^y8f745t*Q@7nG^f&CgYrMzUXcZe**",
+	secret: process.env.SECRET,
 });
