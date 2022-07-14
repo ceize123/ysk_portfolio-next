f93bf9f8e3453f571a9b4add48ef13765db99a4c
ytkuo <hahaeating1@gmail.com>
2022-07-13 21:46:09 -0400

    7/13 auth test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 
+// https://medium.com/@prodmxle/setting-up-nextauth-js-application-and-deploying-it-via-vercel-d6b02bf98397
 export default NextAuth({
 	providers: [
 		GitHubProvider({
