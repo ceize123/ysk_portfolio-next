05746e5255506efd140651e6e5e35e1d76108fcc
ytkuo <hahaeating1@gmail.com>
2022-07-13 21:10:05 -0400

    7/13 auth test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,7 @@ import GitHubProvider from "next-auth/providers/github";
 export default NextAuth({
 	providers: [
 		GitHubProvider({
+			secret: process.env.SECRET,
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET
 		})
