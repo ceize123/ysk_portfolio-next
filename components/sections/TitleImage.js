b00de268fd95180a344c0cd86b9ea459fa2931e7
ytkuo <hahaeating1@gmail.com>
2022-07-20 16:37:30 -0400

    7/20 title image upload multiple imgs

--- components/sections/TitleImage.js
+++ components/sections/TitleImage.js
@@ -8,7 +8,10 @@ function TitleImage({prop, images}) {
 		// style={{ maxWidth: `${imageUrl.width}px` }} make div's max width as image width
 		<section className="relative mx-auto title-image" >
 			<h1 className="text-left lg:ml-12 md:ml-10 ml-5">{data.title}</h1>
-			<ImageRender prop={images[0]} />
+			{/* <ImageRender prop={images[0]} /> */}
+			{images.map((image, idx) => (
+				<ImageRender key={idx} prop={image} />
+			))}
 		</section>
 	);
 }
