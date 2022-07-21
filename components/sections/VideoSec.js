76840215fa24e8d78968b39b1672b84b8b0c6578
ytkuo <hahaeating1@gmail.com>
2022-07-21 18:02:18 -0400

    7/21 video fix

--- components/sections/VideoSec.js
+++ components/sections/VideoSec.js
@@ -10,12 +10,14 @@ function VideoSec({ prop, images }) {
 		<section className="relative video-section mx-5 xl:mx-auto lg:mx-12 md:mx-10" >
 			<h1 className="text-center">{data.title}</h1>
 			<div className="flex justify-center">
-				<ReactPlayer url={data.paragraph} controls={true} width="900px" height="700px" />
-				{/* {windowWidth >= 768
-					? <ReactPlayer url={images[0]} controls={true} width="640px" height="400px" />
-					: <ReactPlayer url={images[0]} controls={true} width="375px" height="200px" />
-				} */}
+				<ReactPlayer url={data.paragraph} controls={true} width="100%" height="auto" />
 			</div>
+			{/* <div className="md:flex justify-center hidden lg:hidden">
+				<ReactPlayer url={data.paragraph} controls={true} width="720px" height="auto" />
+			</div>
+			<div className="flex justify-center md:hidden">
+				<ReactPlayer url={data.paragraph} controls={true} width="350px" height="auto" />
+			</div> */}
 		</section>
 	);
 }
