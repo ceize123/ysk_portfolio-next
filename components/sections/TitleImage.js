3309d2a084af664578a4e09e763e6290d9bea926
ytkuo <hahaeating1@gmail.com>
2022-12-27 12:39:17 -0500

    12/27 test

--- components/sections/TitleImage.js
+++ components/sections/TitleImage.js
@@ -3,7 +3,6 @@ import ImageRender from "../ImageRender";
 function TitleImage({prop, images}) {
 	const data = prop;
 	return (
-		// style={{ maxWidth: `${imageUrl.width}px` }} make div's max width as image width
 		<section className="relative mx-auto title-image" >
 			<h1 className="text-left lg:ml-12 md:ml-10 ml-5">{data.title}</h1>
 			{images.map((image, idx) => (
